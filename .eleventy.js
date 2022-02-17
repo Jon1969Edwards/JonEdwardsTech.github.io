fad033a4de0e4170f9f09bf6803e59a8a5296d90
jonInSweden <jon@vrconstructed.com>
2022-02-17 17:38:03 +0100

    Adds modals and fixes styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy('./dist/css');
+    eleventyConfig.addPassthroughCopy('./dist/css/agency.min.css');
+    eleventyConfig.addPassthroughCopy('./dist/css/styles.css');
+    eleventyConfig.addPassthroughCopy('./dist/css/agency.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy('./scss/base');
-    eleventyConfig.addPassthroughCopy('./scss/components');
-    eleventyConfig.addPassthroughCopy('./scss/layout');
+    eleventyConfig.addPassthroughCopy('./src/js/scripts.js');
+    eleventyConfig.addPassthroughCopy('.src/scss/base');
+    eleventyConfig.addPassthroughCopy('.src/scss/components/_navbar.scss');
+    eleventyConfig.addPassthroughCopy('.src/scss/components/_buttons.scss');
+    eleventyConfig.addPassthroughCopy('.src/scss/layout');
+    eleventyConfig.addPassthroughCopy('./dist/assets/img');
+    eleventyConfig.addPassthroughCopy('./dist/js/scripts.js');
 
     return {
         dir: {
